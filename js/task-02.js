e2561dbaf4d45eb8c20ae18b0995fa0998e36533
Zoja7 <yaozonka@gmail.com>
2023-08-15 09:50:01 +0300

    8_commit

--- js/task-02.js
+++ js/task-02.js
@@ -8,14 +8,31 @@ const ingredients = [
 ];
 
 function renderMarkup(ingredients) {
-  const ul = document.querySelector("#ingredients");
+//   const ul = document.querySelector("#ingredients");
+
+//   const liElElements = [];
+  
+//    ingredients.map( ingredient => {
+//      const liEl = document.createElement("li");
+//      liEl.classList.add("item")
+//      liEl.textContent = ingredient;
+//      liElElements.push(liEl);
+//    });
+//   ul.append(...liElElements); 
+// }  
+// renderMarkup(ingredients);
+
+ const ul = document.querySelector("#ingredients");
 
    ingredients.map( ingredient => {
      const liEl = document.createElement("li");
      liEl.classList.add("item")
      liEl.textContent = ingredient;
-     ul.appendChild(liEl); 
+     return liEl;
+   }).forEach( liEl => {
+       ul.append(liEl); 
    });
+  
 }
     
 renderMarkup(ingredients);
--- js/task-05.js
+++ js/task-05.js
@@ -7,5 +7,15 @@ const textOutput = document.getElementById("name-output");
 // textInput.addEventListener("input", handleOption);
 
 textInput.addEventListener("input", (event)=> {
-    textOutput.textContent = event.currentTarget.value;
+   textOutput.textContent = event.currentTarget.value;
+});
+
+textInput.addEventListener("blur", () => {
+  const inputValue = textInput.value;
+  
+  if (inputValue === "") {
+    textOutput.textContent = "Anonymous";
+  } else {
+    textOutput.textContent = inputValue;
+  }
 });
